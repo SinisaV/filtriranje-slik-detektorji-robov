92008d49df7e6eead9506a63d754d89893ef84ca
sinisaV <sinisa.vucetic@student.um.si>
2023-03-30 09:06:30 +0200

    Added .gitignore, and new image

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+import cv2
+import numpy as np
+
+
+def showImage(name, img):
+    cv2.imshow(name, img)
+
+
 if __name__ == '__main__':
     print("Hello world!")
+
+    i = cv2.imread("lenna.png", 0)
+    showImage("Img: ", i)
+
+    cv2.waitKey()
+    cv2.destroyAllWindows()
