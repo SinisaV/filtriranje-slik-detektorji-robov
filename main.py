aaaaebc1c9b5a3c2bffa34a735abc94626b3118e
sinisaV <sinisa.vucetic@student.um.si>
2023-03-30 09:43:13 +0200

    Implemented canny function

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import cv2
 import numpy as np
 
 
+def canny(slika, sp_prag, zg_prag):
+    slika_robov = cv2.Canny(slika, sp_prag, zg_prag)
+    return slika_robov
+
+
 def showImage(name, img):
     cv2.imshow(name, img)
 
@@ -10,6 +15,9 @@ if __name__ == '__main__':
     print("Hello world!")
 
     i = cv2.imread("lenna.png", 0)
+    showImage("Img: ", i)
+
+    i = canny(i, 80, 20)
     showImage("Img: ", i)
 
     cv2.waitKey()
